26141d4db90dde22e382ffe363ca3f9a787ae1fc
Ruben <rvazquezcaceres@gmail.com>
2019-10-15 10:37:08 +0200

    Vistas y orden de imágenes

---
+++ MemoryGame/Photos.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryGame
+//
+//  Created by alumnos on 15/10/2019.
+//  Copyright © 2019 alumnos. All rights reserved.
+//
+
+import UIKit
+
+class Photos: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
