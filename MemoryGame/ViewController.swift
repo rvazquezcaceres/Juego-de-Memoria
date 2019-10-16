3a5bde83290ef8668ed6406d99b3e313e4bb02c5
Ruben <rvazquezcaceres@gmail.com>
2019-10-16 12:45:53 +0200

    Randomizador

--- MemoryGame/ViewController.swift
+++ MemoryGame/ViewController.swift
@@ -29,31 +29,37 @@ class ViewController: UIViewController {
     }
 
     @IBAction func playerRandom(_ sender: Any) {
+        for _ in 1...4 {
+            changer(segundos: 2)
+            
+        }
         
+    }
+    
+    func changer(segundos: UInt32){
+        sleep(segundos)
         let random = arc4random_uniform(8) //returns 0 to 2 randomly
         
         switch random {
-            case 0:
-                players.image = UIImage (named: "murray.jpg")
-            case 1:
-                players.image = UIImage (named: "nadal.jpg")
-            case 2:
-                players.image = UIImage (named: "thiem.jpg")
-            case 3:
-                players.image = UIImage (named: "djokovic.jpg")
-            case 4:
-                players.image = UIImage (named: "tsitsipas.jpg")
-            case 5:
-                players.image = UIImage (named: "kyrgios.jpg")
-            case 6:
-                players.image = UIImage (named: "medvedev.jpg")
-            case 7:
-                players.image = UIImage (named: "federer.jpg")
-            default:
-                players.image = UIImage (named: "pelota.jpg")
+        case 0:
+            players.image = UIImage (named: "murray.jpg")
+        case 1:
+            players.image = UIImage (named: "nadal.jpg")
+        case 2:
+            players.image = UIImage (named: "thiem.jpg")
+        case 3:
+            players.image = UIImage (named: "djokovic.jpg")
+        case 4:
+            players.image = UIImage (named: "tsitsipas.jpg")
+        case 5:
+            players.image = UIImage (named: "kyrgios.jpg")
+        case 6:
+            players.image = UIImage (named: "medvedev.jpg")
+        case 7:
+            players.image = UIImage (named: "federer.jpg")
+        default:
+            players.image = UIImage (named: "pelota.jpg")
         }
-        
-        
     }
     
 }
